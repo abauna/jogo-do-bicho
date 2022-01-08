9485ef5fbdd63cd651c067daf2f2763863e60501
abauna <48293205+abauna@users.noreply.github.com>
2022-01-08 09:03:44 -0300

    Add files via upload

---
+++ jogo_do_bicho/jogo_do_bicho.js
@@ -0,0 +1,8 @@
+
+function ran()
+{
+  var rand=Math.floor(Math.random() * 25)
+  var bichos=new Array("Avestruz","Águia ","Burro","Borboleta","Cachorro","Cabra","Carneiro","Camelo","Cobra","Coelho","Cavalo","Elefante","Galo","Gato","Jacaré","Leão","Macaco","Porco","Pavão","Peru","Touro","Tigre","Urso","Veado","Vaca")
+  return bichos[rand];
+  }
+
